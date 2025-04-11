2b23f64c43f644c6f8fc572f4ca096fa6b3c7efc
SATYAMP023 <personalphovido@gmail.com>
2025-04-11 12:31:28 +0530

    style update part1

--- client/header.php
+++ client/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-section">
+<nav class="navbar navbar-expand-lg navbar-section">
   <div class="container navbarone" style="max-width: 1235px;">
     <a class="navbar-brand" href="./"><img src="./public/logo.png" alt="" id="logo"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- client/login.php
+++ client/login.php
@@ -1,24 +1,26 @@
-<div class="container">
+<div class="container container-signup">
     <h1 class="heading">Login</h1>
     <form method="POST" action="./server/requests.php" id="login-form">
       
-      <div class="col-6 offset-sm-3 margin-bottom-15">
+    <div class="row">
+      <div class="col-10 offset-sm-1 margin-bottom-15">
         <label for="email" class="form-label">User Email <SUp>*</SUp></label>
         <input type="email" name="email" class="form-control" id="email" placeholder="Enter Your Registered Email" required>
       </div>
       
-      <div class="col-6 offset-sm-3 margin-bottom-15">
+      <div class="col-10 offset-sm-1 margin-bottom-15">
         <label for="password" class="form-label">Password <SUp>*</SUp></label>
         <input type="password" name="password" class="form-control" id="password" placeholder="Enter your password" required>
-
+        
         <div class="text-end mt-2">
           <a href="index.php?forgot-password" class="text-decoration-none">Forgot Password?</a>
         </div>
       </div>
-  
+    </div>
+      
   <div class="signup-button">
     
-    <div class="col-2 offset-sm-3">
+    <div class="col-2 offset-sm-9">
       <button type="submit" name="login" class="btn btn-primary">login</button>
     </div>
     
